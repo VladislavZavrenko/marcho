389cd281437aa983ecb3467293f64a0db7744a35
VladislavZavrenko <nigribetsky7@gmail.com>
2022-11-24 19:02:30 +0300

    added module index

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-const { parallel, dest, series } = require('gulp');
-const gulp = require('gulp');
-const sass = require('gulp-sass')(require('sass'));
-const scss = require('gulp-sass');
-const concat = require('gulp-concat');
-const autoprefixer = require('gulp-autoprefixer');
-const uglify = require('gulp-uglify');
-const browserSync = require('browser-sync').create();
-const del = require('del');
+const { parallel, dest, series, src } = require('gulp');
+const gulp            = require('gulp');
+const sass            = require('gulp-sass')(require('sass'));
+const scss            = require('gulp-sass');
+const concat          = require('gulp-concat');
+const autoprefixer    = require('gulp-autoprefixer');
+const uglify          = require('gulp-uglify');
+const rename          = require('gulp-rename');
+const nunjucksRender  = require('gulp-nunjucks-render');
+const browserSync     = require('browser-sync').create();
+const del             = require('del');
 
 
 gulp.task('browsersync', function () {
@@ -17,19 +19,38 @@ gulp.task('browsersync', function () {
   });
 });
 
+// gulp.task('default', function() {
+//   return gulp.src('src/app/*.html')
+//     .pipe(nunjucksRender({
+//       path: ['src/app/'] 
+//     }))
+//     .pipe(gulp.dest('dist'))
+//     .pipe(browserSync.stream());
+// });
+
+function nunjucks() {
+  return src('app/*.njk')
+  .pipe(nunjucksRender())
+  .pipe(dest('app'))
+  .pipe(browserSync.stream());
+}
+
 function build() {
   return gulp.src([
     'app/**/*.html',
     'app/css/style.min.css',
     'app/js/main.min.js'
   ], { base: 'app' })
-    .pipe(gulp.dest('dist'));
+  .pipe(gulp.dest('dist'));
 };
 
 function styles() {
-  return gulp.src('app/scss/style.scss')
+  return gulp.src('app/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(concat('style.min.css'))
+    // .pipe(concat())
+    .pipe(rename({
+    suffix: '.min'
+  }))
     .pipe(autoprefixer({
       overrideBrowserslist: ['Last 10 versions'],
       grid: true
@@ -66,16 +87,18 @@ exports.styles = styles;
 exports.cleanDist = cleanDist;
 exports.build = series(cleanDist, build);
 exports.scripts = scripts;
+exports.nunjucks = nunjucks;
 exports.watch = function () {
-  gulp.watch('app/scss/**/*.scss', styles);
+  gulp.watch(['app/scss/**/*.scss'], styles);
+  gulp.watch(['app/*.njk'], nunjucks);
   gulp.watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts);
   gulp.watch(['app/**/*.html']).on('change', browserSync.reload);
-
 };
 
 
 exports.default = parallel(
   styles,
+  nunjucks,
   scripts,
   async function watch() {
     gulp.watch('app/scss/**/*.scss', styles);
